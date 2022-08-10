a31dd8a7e6e1a44250ff67e89f58df31302e5d5a
AryanBagheri <95540857+AryanBagheri@users.noreply.github.com>
2022-08-10 14:36:17 +0430

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace cSharp6
+{
+
+    public interface Building
+    {
+        public string Construction();
+    }
+    public class Apartment : Building
+    {
+        public string Construction()
+        {
+            return "here is your apartment. ";
+        }
+    }
+    public class House : Building
+    {
+        public string Construction()
+        {
+            return "here is your house. ";
+        }
+    }
+
+    abstract class BuildingConstruction
+    {
+        public abstract Building FactoryOfBuilding();
+
+        public string ConstructionResul()
+        {
+            var building = FactoryOfBuilding();
+            var result = building.Construction();
+            return result;
+        }
+    }
+    class ApartmentBuilding : BuildingConstruction
+    {
+        public override Building FactoryOfBuilding()
+        {
+            return new Apartment();
+        }
+    }
+    class HouseBuilding : BuildingConstruction
+    {
+        public override Building FactoryOfBuilding()
+        {
+            return new House();
+        }
+    }
+
+
+    class Customer
+    {
+        public void Main()
+        {
+            CustomersPick(new HouseBuilding());
+        }
+        public void CustomersPick(BuildingConstruction pick)
+        {
+            Console.WriteLine(pick.ConstructionResul());
+        }
+    }
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+            new Customer().Main();
+        }
+    }
+
+
+}
